497138982c2c956473c5339316de1a0324c97dda
Christian <christian@koehlke.de>
2022-06-30 23:49:53 +0200

    refactoring

--- Arduino/include/ForceSensor.hpp
+++ Arduino/include/ForceSensor.hpp
@@ -2,28 +2,6 @@
 
 #include <Wire.h> //For I2C/SMBus
 
-/*-----------------------------------------------------------------------------
- * SingleTact I2C Demo
- *
- * Copyright (c) 2016 Pressure Profile Systems
- * Licensed under the MIT license. This file may not be copied, modified, or
- * distributed except according to those terms.
- *
- *
- * Demonstrates sensor input by reading I2C and display value on comm port.
- *
- * The circuit: as described in the manual for PC interface using Arduino.
- *
- * To compile: Sketch --> Verify/Compile
- * To upload: Sketch --> Upload
- *
- * For comm port monitoring: Click on Tools --> Serial Monitor
- * Remember to set the baud rate at 57600.
- *
- * September 2016
- * ----------------------------------------------------------------------------- */
-
-
 #define FORCE_LEVER_RATIO 2.16667
 
 
